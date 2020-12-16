4393bcb405840f29da97384d87b02aaa2202d88e
HuyTon <tonthatminhh@gmail.com>
2020-12-16 22:15:17 +0700

    ...

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,18 +2,20 @@ const mongoose = require('mongoose')
 const email = require('mongoose-slug-generator')
 const password = require('mongoose-slug-generator')
 const role = require('mongoose-slug-generator')
+const phone = require('mongoose-slug-generator')
 mongoose.plugin(email)
 mongoose.plugin(password)
 mongoose.plugin(role)
+mongoose.plugin(phone)
 
 const Schema = mongoose.Schema
 
 const User = new Schema({
 
-    email: { type: String, minlength: 6, required: true, unique: true },
+    email: { type: String, minlength: 6, required: true },
     password: { type: String, minlength: 6, required: true },
     username: { type: String, minlength: 6, required: true },
-    phone: { type: String, minlength: 6, required: true },
+    phone: { type: Number, minlength: 6, required: true, unique: true },
     // gender: { type: String, minlength: 6, required: true },
     role: { type: String, required: true }
 },
